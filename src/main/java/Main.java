d78420d8bcf58d1e48e4a6a5b8d023c98668f8f3
Wesley-Fabrizio-Zomer <160271969+Wesley-Fabrizio-Zomer@users.noreply.github.com>
2024-03-20 18:53:32 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+//Ola mundo em java 
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
